07a59163bf91a06aaec0e8ee7617e118e61778d9
VitalyL <vetallrssn@yandex.ru>
2025-08-25 19:49:46 +0300

    Добавить поле с entityId в NotFoundException

--- server/src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ server/src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public class NotFoundException extends RuntimeException {
     private final String entityName;
+    private final Long entityId;
 
     public NotFoundException(String entityName, Long id) {
         super(entityName + " with id " + id + " not found");
         this.entityName = entityName;
+        this.entityId = id;
     }
 }
